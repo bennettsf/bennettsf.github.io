00bf14e3746cdb450a2636f9ec6a0dca75cc04ed
Bennett Fife <bennett.fife@gmail.com>
2025-12-09 23:09:21 -0800

    tag filtering logic pretty much done

--- src/components/Projects/ProjectTags.tsx
+++ src/components/Projects/ProjectTags.tsx
@@ -2,16 +2,29 @@ import { useSelectedTags } from '@/context/SelectedTagsContext';
 
 import { getTopTags } from '@/hooks/getTopTags';
 import { Box, Stack, Tag } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import React from 'react';
 import { HiPlus } from 'react-icons/hi';
 
 function ProjectTags({}) {
   const { selectedTags, setSelectedTags } = useSelectedTags();
+  const topTags = getTopTags();
+  const tags = ['All', ...topTags];
 
-  useEffect(() => {
-    const tags = getTopTags();
-    setSelectedTags(['All', ...tags]);
-  }, []);
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const tag = e.currentTarget.innerText;
+    if (tag === 'All') {
+      setSelectedTags(['All']);
+      return;
+    }
+
+    // add tag and remove "All" or remove tag and add "All" if no tags left
+    if (!selectedTags.includes(tag)) {
+      setSelectedTags([...selectedTags.filter((t) => t !== 'All'), tag]);
+    } else {
+      const newTags = selectedTags.filter((t) => t !== tag);
+      setSelectedTags(newTags.length === 0 ? ['All'] : newTags);
+    }
+  };
 
   return (
     <Box>
@@ -23,8 +36,14 @@ function ProjectTags({}) {
         justifyContent="center"
         flexWrap="wrap"
       >
-        {selectedTags.map((tag) => (
-          <Tag.Root key={tag} size="lg" colorPalette="teal">
+        {tags.map((tag) => (
+          <Tag.Root
+            key={tag}
+            size="lg"
+            colorPalette={!selectedTags.includes(tag) ? 'teal' : 'gray'}
+            variant="solid"
+            onClick={handleClick}
+          >
             <Tag.StartElement>
               <HiPlus />
             </Tag.StartElement>
--- src/components/Projects/ProjectsSection.tsx
+++ src/components/Projects/ProjectsSection.tsx
@@ -4,12 +4,19 @@ import type { Project } from '@/models/Project';
 import projectsData from '@/data/projects.json';
 import ProjectTags from './ProjectTags';
 import ProjectCard from './ProjectCard';
+import { useSelectedTags } from '@/context/SelectedTagsContext';
 
 const ProjectsSection = () => {
+  const { selectedTags } = useSelectedTags();
   // order projects by featured status
   const projects = [...projectsData].sort((a, b) =>
     b.featured === a.featured ? 0 : b.featured ? 1 : -1
   ) as Project[];
+
+  const filteredProjects =
+    selectedTags.includes('All') || selectedTags.length === 0
+      ? projects
+      : projects.filter((project) => project.tags.some((tag) => selectedTags.includes(tag)));
 
   return (
     <FullScreenSection
@@ -24,7 +31,7 @@ const ProjectsSection = () => {
         <ProjectTags />
         {/* Render project cards here using the projects state */}
         <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6}>
-          {projects.map((project) => (
+          {filteredProjects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </Grid>
