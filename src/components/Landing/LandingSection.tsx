ad23a7d9c19719e7baacb51f45ace360a663a40b
Bennett Fife <bennett.fife@gmail.com>
2025-12-10 00:40:18 -0800

    restructure and edit landing page text

--- src/components/Landing/LandingSection.tsx
+++ src/components/Landing/LandingSection.tsx
@@ -48,20 +48,21 @@ const LandingSection = () => {
           </Text>
           <Box display="flex" flexDirection="column" gap={10}>
             <Text className={slideInDelayedClass} fontSize="xl" maxWidth="600px">
+              I'm from <strong>Seattle, WA</strong> and some of my interests include{' '}
+              <strong>coding</strong>, <strong>gaming</strong>, <strong>movies</strong>, and{' '}
+              <strong>baseball</strong>.
+            </Text>
+            <Text className={slideInDelayedClass} fontSize="xl" maxWidth="600px">
               I specialize in building web applications using modern technologies like{' '}
               <strong>React</strong>, <strong>Node.js</strong>, and <strong>TypeScript</strong>
               .{' '}
             </Text>
             <Text className={slideInDelayedClass} fontSize="xl" maxWidth="600px">
-              Below, you can find some of the projects I've worked on as well as a fun word game I
-              built!
+              Below, you can find some of the <u>projects</u> I've worked on as well as a fun{' '}
+              <u>word game</u> I built!
             </Text>
             <Text className={slideInDelayedClass} fontSize="xl" maxWidth="600px">
-              I'm from <strong>Seattle, WA</strong> and some of my interests include{' '}
-              <strong>hiking</strong>, <strong>gaming</strong>, and <strong>baseball</strong>.
-            </Text>
-            <Text className={slideInDelayedClass} fontSize="xl" maxWidth="600px">
-              Feel free to reach out to me or connect through any of my socials above!
+              Feel free to reach out to me through any of my socials above!
             </Text>
           </Box>
         </Box>
